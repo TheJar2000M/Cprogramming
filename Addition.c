6f0afc279b5fa9cd7ec5238f3920026b4afe45d2
TheJar2000M <s6302041620052@email.kmutnb.ac.th>
2021-01-18 19:08:03 +0700

    first commit

---
+++ Addition.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// ดึง input 
+
+void main(){
+//  ส่วนที่2 
+    int num1, num2, total;
+    // ต้าวตัวแปร กับ total
+    printf("value of num1 is :");
+scanf("%d" ,&num1);
+// %d = ตัวเลข
+    
+    printf("value of num2 is :");
+scanf("%d" ,&num2);
+
+total =num1 + num2 ;
+printf("%d",total);
+
+
+}
